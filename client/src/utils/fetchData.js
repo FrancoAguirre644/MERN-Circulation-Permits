a3c777549294b152dddd22d8cb1bfdb185782f89
FrancoAguirre644 <francoaguirre644@gmail.com>
2021-08-23 02:18:38 -0300

    .env added to Frontend

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:5000/api/v1"
+const baseURL = process.env.REACT_APP_API
 
 export const getData = async (url, token) => {
     const res = await fetch(`${baseURL}/${url}`, {
